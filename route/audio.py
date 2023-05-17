c9973f58e734f52830d46a1301ec2ec0048178c9
pradeep <pradeepmca0392@gmail.com>
2023-05-17 11:53:26 +0530

    Added api list

--- route/audio.py
+++ route/audio.py
@@ -7,6 +7,7 @@ from schemas.audio import LinkProcess
 from ml_process.audio_mapper import AudioMapper
 from ml_process.media_conversion import VideoAudioExtractor
 import os
+import pickle
 
 router = APIRouter(
     prefix='/Audio',
@@ -73,4 +74,44 @@ async def upload_media_file(
 @router.get("/get_models")
 async def get_models():
     file_list = os.listdir(os.getcwd() + '/models') 
-    print(file_list)
+    file_list = [file.split('.')[0] for file in file_list]
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content= {"message": "Model list successfully fetched", "model_list": file_list}
+    )
+
+
+@router.get("/select_model")
+async def select_model(model_name):
+    file_path = os.path.join(os.getcwd(),'models',model_name+'.pkl')
+    model = pickle.load(open(file_path, 'rb'))  if os.path.isfile(file_path) else None
+    if not model:
+        return JSONResponse(
+            status_code= status.HTTP_400_BAD_REQUEST,
+            content= {"message": "Model name not found"},
+        )
+    return JSONResponse(
+        status_code= status.HTTP_200_OK,
+        content= {"data": model},
+    )
+
+
+@router.get("/download_media")
+async def download_media():
+    media_file_path = ""
+    if not os.path.isfile(media_file_path):
+        return JSONResponse(
+            status_code= status.HTTP_400_BAD_REQUEST,
+            content= {"message": "Media file not found"},
+        )
+    return JSONResponse(
+        status_code= status.HTTP_200_OK,
+        content= {"message": "Media file downloaded successfully"}
+    )
+
+
+# @router.post("/qa")
+# async def get_answer(data: QA):
+#     obj = pkl.load(os.path.join(os.getcwd(), "files", "trained", str(data.model).strip()+".pkl"))
+#     method, answer = model_obj.model1_qa(data.question, obj.get("docsearch"), obj.get("chain"))
+#     return {"method": method ,"answer": answer}
